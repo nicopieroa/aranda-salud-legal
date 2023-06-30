5cdef2b3a97b09af68264bca757e4fb028b42037
Nicolas Pieroni Aranda <nicolaspieroniaranda@gmail.com>
2023-06-30 20:20:25 -0300

    mantine library installed, and setting the app ready to start developing

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import { MantineProvider } from '@mantine/core';
 
 function App() {
 
   return (
-    <>
-    </>
+    <MantineProvider withGlobalStyles withNormalizeCSS>
+      <h1>HALAAA!</h1>
+    </MantineProvider>
   )
 }
 
