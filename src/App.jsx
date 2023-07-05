d3ccedb6a5a4c213e52697e73f2a3a179aa23698
Nicolas Pieroni Aranda <nicolaspieroniaranda@gmail.com>
2023-07-05 17:23:34 -0300

    contact section basic design and errors control, done

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { MantineProvider } from '@mantine/core';
 import { Navbar } from './components/navbar';
 import { Header } from './components/header';
 import { Main } from './components/main';
+import { ContactSection } from './components/contactSection';
 
 function App() {
 
@@ -13,6 +14,7 @@ function App() {
         <Navbar />
         <Header />
         <Main />
+        <ContactSection />
       </div>
 
     </MantineProvider>
---
+++ src/components/contactSection.jsx
@@ -0,0 +1,51 @@
+import { useForm } from '@mantine/form';
+import { TextInput, Textarea, Button } from '@mantine/core';
+
+export function ContactSection() {
+    const form = useForm({
+        initialValues: { name: '', email: '', textArea: '' },
+
+        // functions will be used to validate values at corresponding key
+        validate: {
+            name: (value) => (value.length < 2 ? 'Ingrese su nombre' : null),
+            email: (value) => (/^\S+@\S+$/.test(value) ? null : 'Email invalido'),
+            textArea: (value) => (value.length < 10 ? 'Ingrese su mensaje' : null),
+        },
+    });
+
+    return (
+        <div className='contactSection'>
+            <h2>Contactanos</h2>
+
+            <form onSubmit={form.onSubmit(console.log)} className='form'>
+                <TextInput
+                    label="Nombre"
+                    placeholder="Gustavo"
+                    radius="md"
+                    withAsterisk
+                    {...form.getInputProps('name')}
+                    style={{ margin: "0" }} />
+                <TextInput
+                    mt="sm"
+                    label="Email"
+                    placeholder="gustavo@email.om"
+                    radius="md"
+                    withAsterisk
+                    {...form.getInputProps('email')}
+                    style={{ margin: "0" }} />
+
+                <Textarea
+                    placeholder="Mi consulta es sobre..."
+                    label="Tu mensaje"
+                    radius="md"
+                    withAsterisk
+                    {...form.getInputProps('textArea')}
+                />
+
+                <Button type="submit" mt="sm" radius="md">
+                    ENVIAR
+                </Button>
+            </form>
+        </div>
+    );
+}
