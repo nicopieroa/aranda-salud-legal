fb404cc738cd11ccf96ee80b4321f02ad64d069e
Nicolas Pieroni Aranda <nicolaspieroniaranda@gmail.com>
2023-07-10 18:15:37 -0300

    get  data from json, done

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,3 +1,5 @@
+import articles from '../assets/articles-data.json'
+
 import { Carousel } from '@mantine/carousel';
 import { Cards } from './newsCard';
 import { Title } from '@mantine/core';
@@ -6,7 +8,7 @@ export function Main() {
 
     return (
         <main className='main'>
-            <Title order={2} size="h2" color="#ffffff">Noticias</Title>
+            <Title order={2} size="32px" color="#ffffff">Noticias</Title>
 
             <Carousel
                 slideSize="80%"
@@ -18,9 +20,9 @@ export function Main() {
                 dragFree
                 maw="100%"
             >
-                <Carousel.Slide><Cards /></Carousel.Slide>
-                <Carousel.Slide><Cards /></Carousel.Slide>
-                <Carousel.Slide><Cards /></Carousel.Slide>
+                {articles.map((article) =>
+                    <Carousel.Slide key={article.id}><Cards article={article} /></Carousel.Slide>
+                )}
             </Carousel>
         </main>
     )
