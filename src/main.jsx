83a867381449fd5d8cd07429d947cf1fe36cd954
Nicolas Pieroni Aranda <nicolaspieroniaranda@gmail.com>
2023-07-17 20:28:55 -0300

    router file, created

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-import { Route } from 'wouter'
-
 import './index.css'
-import App from './App.jsx'
-import { AboutUsPage } from './pages/aboutUsPage'
-import { ServicesPage } from './pages/servicesPage'
-
-function Router() {
-  return (
-    <div>
-      <Route path='/' component={App} />
-      <Route path='/quienes-somos' component={AboutUsPage} />
-      <Route path='/nuestros-servicios' component={ServicesPage} />
-    </div>
-  )
-}
+import { Router } from './routes/router'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
