6b51e578ac422c0acf4c8a758c54f6c3fc4bd46c
Nicolas Pieroni Aranda <nicolaspieroniaranda@gmail.com>
2023-07-28 18:34:50 -0300

    navbar function on scroll, done. Burger button doesnt loock so clean whe sidebar close

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,10 +3,31 @@ import { Burger, Drawer } from '@mantine/core';
 
 import { Link } from "wouter"
 
+import { useEffect, useState } from 'react';
+
 export function Navbar() {
     const [opened, { toggle }] = useDisclosure(false);
-
     const label = opened ? 'Close navigation' : 'Open navigation';
+
+    const [prevScrollPos, setPrevScrollPos] = useState(0);
+    const [visible, setVisible] = useState(true);
+
+    const handleScroll = () => {
+        const currentScrollPos = window.scrollY;
+
+        setVisible(prevScrollPos > currentScrollPos);
+        setPrevScrollPos(currentScrollPos);
+    };
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [prevScrollPos, visible]);
+
 
     function Sidebar() {
         return (
@@ -16,13 +37,23 @@ export function Navbar() {
                 position="right"
                 size='75%'
                 color='black'
-                variant='green'
-                transitionProps={{
-                    transition: 'fade',
-                    duration: 300,
-                    timingFunction: 'linear'
-                }}>
+                variant='green'>
                 <aside className="sidebar">
+                    {opened ? <Burger
+                        opened={opened}
+                        onClick={toggle}
+                        aria-label={label}
+                        size="lg"
+                        color=' #ffffff'
+                        className='burgerButton-sidebar' />
+                        : <Burger
+                            opened={opened}
+                            onClick={toggle}
+                            aria-label={label}
+                            size="lg"
+                            color='#085db2'
+                            className='burgerButton' />}
+
                     <Link href='/quienes-somos'><a>Quiénes somos</a></Link>
                     <Link href='/nuestros-servicios'><a>Nuestros servicios</a></Link>
                 </aside>
@@ -32,7 +63,7 @@ export function Navbar() {
 
     return (
         <>
-            <nav className='navbar'>
+            <nav className={`navbar ${visible ? 'navbar-visible' : 'navbar-hidden'}`}>
                 <Link href='/'>
                     <a>
                         <img
@@ -56,9 +87,11 @@ export function Navbar() {
                         size="lg"
                         color='#085db2'
                         className='burgerButton' />}
+
             </nav>
 
             {opened ? <Sidebar /> : null}
+
         </>
     )
 }
